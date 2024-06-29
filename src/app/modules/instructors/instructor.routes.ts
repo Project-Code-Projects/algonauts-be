fd26b011bd7ab9734e219b752704f512077e6aa8
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-06-29 14:31:06 +0600

    feat: authguard enabled

--- src/app/modules/instructors/instructor.routes.ts
+++ src/app/modules/instructors/instructor.routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { InstructorController } from './instructor.controller';
+import auth from '../../middlewares/auth';
+import { UserType } from '../users/user.interface';
 
 const router = express.Router();
 
-router.post('/create', InstructorController.createInstructor);
+router.post(
+  '/create',
+
+  InstructorController.createInstructor,
+);
 router.put('/update/:id', InstructorController.updateInstructor);
 router.delete('/delete/:id', InstructorController.deleteInstructor);
 router.get('/exercise-statistics', InstructorController.getExerciseStatistics);
@@ -11,8 +17,14 @@ router.get(
   '/exercise-statistics/:studentId',
   InstructorController.getExerciseStatisticsByStudentId,
 );
+// router.get(
+//   '/student-code-snippet/:studentId',
+//   auth(UserType.Instructor),
+//   InstructorController.getStudentCodeSnippets,
+// );
 router.get(
   '/student-code-snippet/:studentId',
+  auth(UserType.Instructor),
   InstructorController.getStudentCodeSnippets,
 );
 router.get('/user/:userId', InstructorController.getInstructorsByUser);
